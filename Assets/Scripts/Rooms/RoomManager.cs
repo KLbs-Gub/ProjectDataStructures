c0911a1ce05388b73c5d40deb186299453db45d7
Kieran <87498730+KLbs-Gub@users.noreply.github.com>
2024-10-26 21:54:35 -0500

    Room layout generation
    Rooms can now generate with their level's list of possible room layouts.

--- Assets/Scripts/Rooms/RoomManager.cs
+++ Assets/Scripts/Rooms/RoomManager.cs
@@ -8,6 +8,13 @@ public class RoomManager : MonoBehaviour
     // Room manager variables
     [SerializeField] private Room room;
 
+    public List<Level1ScriptObject> levels;
+    public int currentLevel = 0;
+
+    private List<GameObject> normalRooms;
+    private List<GameObject> shopRooms;
+    private List<GameObject> bossRooms;
+
     [Range(1, 15)][SerializeField] private int columns;
     [Range(1, 15)][SerializeField] private int rows;
     private Vector3 transformOffset;
@@ -15,6 +22,9 @@ public class RoomManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        PopulateRoomLists();
+
+        // Run this last
         GenerateRooms();
     }
 
@@ -34,12 +44,34 @@ public class RoomManager : MonoBehaviour
                 transformOffset = new Vector3(17.8f * i, 10 * j, 0);
                 Room newRoom = Instantiate(room, transformOffset, transform.rotation);
 
+                AddLayout("normal");
+
                 // Make sure rooms on the edges have holes to the void blocked off.
                 if (j == rows - 1) newRoom.EnableBlocker("EntranceUp");
                 if (i == 0) newRoom.EnableBlocker("EntranceLeft");
                 if (j == 0) newRoom.EnableBlocker("EntranceDown");
                 if (i == columns - 1) newRoom.EnableBlocker("EntranceRight");
             }
+        }
+    }
+
+    // Get the rooms from the level scriptable objects
+    private void PopulateRoomLists()
+    {
+        normalRooms = levels[currentLevel].normalRoomLayoutsList;
+        shopRooms = levels[currentLevel].shopRoomLayoutsList;
+        bossRooms = levels[currentLevel].bossRoomLayoutsList;
+    }
+
+    // Add a room layout to the room at the transform offset.
+    private void AddLayout(string layoutType)
+    {
+        int roomSelect;
+        if (layoutType == "normal")
+        {
+            roomSelect = Random.Range(0, normalRooms.Count);
+
+            GameObject layout = Instantiate(normalRooms[roomSelect], transformOffset, transform.rotation);
         }
     }
 }
