8ee51dd9d762ce07918777432f304fd4293edc3d
Kieran <87498730+KLbs-Gub@users.noreply.github.com>
2024-10-27 00:53:43 -0500

    Special rooms added to generation
    The following room types now generate:
    * Player spawn room
    * Shop room
    * Boss room

--- Assets/Scripts/Rooms/RoomManager.cs
+++ Assets/Scripts/Rooms/RoomManager.cs
@@ -22,23 +22,60 @@ public class RoomManager : MonoBehaviour
         GenerateRooms();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     // Self explanatory
     private void GenerateRooms()
     {
+        // Variables used to determine when to generate a special room.
+        int playerRoomCountdown = Random.Range(0, rows + columns);
+        int shopRoomCountdown = Random.Range(0, rows + columns);
+        int bossRoomCountdown = Random.Range(0, rows + columns);
+
         for (int i = 0; i < columns; i++)
         {
             for (int j = 0; j < rows; j++)
             {
+                // Count down special room types
+                playerRoomCountdown--;
+                shopRoomCountdown--;
+                bossRoomCountdown--;
+
                 transformOffset = new Vector3(17.8f * i, 10 * j, 0);
                 Room newRoom = Instantiate(room, transformOffset, transform.rotation);
 
-                AddLayout("normal");
+                #region RoomGen else/if chain
+                // If a special room count is below zero, generate that special room type.
+                // We also set the count to an absurdly high number so it doesn't show up again.
+                if (playerRoomCountdown < 0)
+                {
+                    // Don't generate a layout for player spawn room
+                    // but DO move the player to this room.
+                    GameObject player = GameObject.Find("Player");
+                    player.transform.position = transformOffset;
+
+                    // Move the camera too
+                    GameObject camera = GameObject.Find("Main Camera");
+                    camera.GetComponent<CameraMain>().TargetPoint = new Vector3(transformOffset.x, transformOffset.y, 1);
+                    camera.transform.position = transformOffset;
+
+                    playerRoomCountdown = 99999;
+                }
+                else if (shopRoomCountdown < 0)
+                {
+                    AddLayout("shop");
+
+                    shopRoomCountdown = 99999;
+                }
+                else if (bossRoomCountdown < 0)
+                {
+                    AddLayout("boss");
+
+                    bossRoomCountdown = 99999;
+                }
+                else
+                {
+                    AddLayout("normal");
+                }
+                #endregion
 
                 // Make sure rooms on the edges have holes to the void blocked off.
                 if (j == rows - 1) newRoom.EnableBlocker("EntranceUp");
@@ -58,6 +95,18 @@ public class RoomManager : MonoBehaviour
             roomSelect = Random.Range(0, levels[currentLevel].normalRoomLayoutsList.Count);
 
             GameObject layout = Instantiate(levels[currentLevel].normalRoomLayoutsList[roomSelect], transformOffset, transform.rotation);
+        }
+        else if (layoutType == "shop")
+        {
+            roomSelect = Random.Range(0, levels[currentLevel].shopRoomLayoutsList.Count);
+
+            GameObject layout = Instantiate(levels[currentLevel].shopRoomLayoutsList[roomSelect], transformOffset, transform.rotation);
+        }
+        else if (layoutType == "boss")
+        {
+            roomSelect = Random.Range(0, levels[currentLevel].bossRoomLayoutsList.Count);
+
+            GameObject layout = Instantiate(levels[currentLevel].bossRoomLayoutsList[roomSelect], transformOffset, transform.rotation);
         }
     }
 }
