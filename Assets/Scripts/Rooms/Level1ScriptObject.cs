21bbf2f9ea09f7941550e8e3aa4a19c039ac632b
Kieran <87498730+KLbs-Gub@users.noreply.github.com>
2024-10-26 19:56:18 -0500

    Scriptable objects for levels
    Levels can be defined via scriptable objects. This lets us specify the rooms layouts that can be used in each level, as well as to specify the level's name!

--- Assets/Scripts/Rooms/Level1ScriptObject.cs
+++ Assets/Scripts/Rooms/Level1ScriptObject.cs
@@ -5,4 +5,70 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Level_", menuName = "Scriptable Objects/Levels/Level")]
 public class Level1ScriptObject : ScriptableObject
 {
+    #region Header LEVEL DETAILS
+
+    [Space(10)]
+    [Header("LEVEL DETAILS")]
+
+    #endregion
+
+    #region Tooltip
+    [Tooltip("The level's name")]
+    #endregion Tooltip
+
+    public string levelName;
+
+    #region Header LEVEL ROOMS
+    [Space(10)]
+    [Header("LEVEL ROOMS")]
+    #endregion Header LEVEL ROOMS
+    #region Tooltip
+    [Tooltip("The room layouts that can be generated for normal rooms in this level")]
+    #endregion Tooltip
+
+    public List<GameObject> normalRoomLayoutsList;
+
+    #region Tooltip
+    [Tooltip("The room layouts that can be generated for shop rooms in this level")]
+    #endregion Tooltip
+    public List<GameObject> shopRoomLayoutsList;
+
+    #region Tooltip
+    [Tooltip("The room layouts that can be generated for boss rooms in this level")]
+    #endregion Tooltip
+    public List<GameObject> bossRoomLayoutsList;
+
+    #region Validation
+
+#if UNITY_EDITOR
+    // Validate details entered
+    private void OnValidate()
+    {
+        string message = "";
+        if (levelName == "")
+        {
+            message += this + " has unspecified level name!\n";
+        }
+        if (normalRoomLayoutsList.Count == 0)
+        {
+            message += this + " has no specified normal room layouts\n";
+        }
+        if (shopRoomLayoutsList.Count == 0)
+        {
+            message += this + " has no specified shop room layouts\n";
+        }
+        if (bossRoomLayoutsList.Count == 0)
+        {
+            message += this + " has no specified boss room layouts\n";
+        }
+
+        if (message != "")
+        {
+            Debug.LogWarning(message);
+        }
+    }
+
+#endif
+
+    #endregion Validation
 }
