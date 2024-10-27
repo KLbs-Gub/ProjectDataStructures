e2968d71dcadee0cbb3b3e1f628a92dec065554a
Kieran <87498730+KLbs-Gub@users.noreply.github.com>
2024-10-26 19:13:26 -0500

    Removed RoomLayout scriptable object
    It wasn't necessary for our project.

--- Assets/Scripts/Rooms/RoomLayoutScriptObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Room_", menuName = "Scriptable Objects/Levels/Room")]
-public class RoomLayoutScriptObject : ScriptableObject
-{
-    [HideInInspector] public string globalUniqueIdentifierField;
-}
