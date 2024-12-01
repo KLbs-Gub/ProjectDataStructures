5c6cb343b600fbce50050bd9d8aadf94a451f72f
Kieran <87498730+KLbs-Gub@users.noreply.github.com>
2024-12-01 01:21:58 -0600

    Fake pathfinding & physics tweaks
    I created a faux pathfinding system by having enemies move in the direction they last saw you until they see you again. I also created a material that stops objects from slowing down when they move against a wall, which I applied to the enemies and players.

--- Assets/Scripts/Enemy/ChaseEnemy.cs
+++ Assets/Scripts/Enemy/ChaseEnemy.cs
@@ -1,5 +1,6 @@
 //Written by Matthew Kopel
 // 10/27/24
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TreeEditor;
@@ -12,6 +13,15 @@ public class ChaseEnemy : EnemyBase
     public LayerMask mask;
     public float moveSpeed = 1f;
 
+    private int direction = 1;
+    private Rigidbody2D rb;
+    private Vector2 moveDirection = new Vector2(0, 0);
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,19 +31,15 @@ public class ChaseEnemy : EnemyBase
     // Update is called once per frame
     void Update()
     {
-        if (target.transform.position.x < transform.position.x)
-        {
-            transform.localScale = new Vector2(-1, 1);
-        }
-        else
-        {
-            transform.localScale = new Vector2(1, 1);
-        }
+        transform.localScale = new Vector2(direction, 1);
     }
 
     private void FixedUpdate()
     {
+        // trueTarget is the point of the actual hitbox
+        // this is needed for line of sight checks to work.
         Vector3 trueTarget = new Vector3(target.transform.position.x, target.transform.position.y - 0.37f, target.transform.position.z);
+
         // From 'How to make Line of Sight in Unity 2D with Raycast' tutorial
         RaycastHit2D isBlocked = Physics2D.Raycast(this.transform.position, trueTarget - this.transform.position, 4000f, mask);
 
@@ -44,10 +50,22 @@ public class ChaseEnemy : EnemyBase
                 //takes position of player and always moves towards it
                 transform.position = Vector2.MoveTowards(this.transform.position, trueTarget,
                     moveSpeed * Time.deltaTime);
+                if (target.transform.position.x < transform.position.x)
+                {
+                    direction = -1;
+                }
+                else
+                {
+                    direction = 1;
+                }
                 Debug.DrawRay(this.transform.position, trueTarget - this.transform.position, Color.yellow);
+
+                moveDirection.x = direction;
+                moveDirection.y = -MathF.Sign((transform.position.y * transform.position.y) - (trueTarget.y * trueTarget.y));
             }
             else
             {
+                rb.velocity = moveDirection * moveSpeed;
                 Debug.DrawRay(this.transform.position, trueTarget - this.transform.position, Color.red);
             }
         }
